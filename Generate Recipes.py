4e4457eb9036aef34c47d29163766b1356543cb2
Gianluca O <gianopp@gmail.com>
2022-08-31 23:51:08 -0400

    Added count per item

--- Generate Recipes.py
+++ Generate Recipes.py
@@ -1,35 +1,35 @@
 import json
 
-recipes = [
-		"cobbled_deepslate",
-		"polished_deepslate",
-		"deepslate_bricks",
-		"cracked_deepslate_bricks",
-		"deepslate_tiles",
-		"cracked_deepslate_bricks",
-		"chiseled_deepslate",
-		"cobbled_deepslate_wall",
-		"polished_deepslate_wall",
-		"deepslate_brick_wall",
-		"deepslate_tile_wall",
-		"cobbled_deepslate_stairs",
-		"polished_deepslate_stairs",
-		"deepslate_brick_stairs",
-		"deepslate_tile_stairs",
-		"cobbled_deepslate_slab",
-		"polished_deepslate_slab",
-		"deepslate_brick_slab",
-		"deepslate_tile_slab"
-	]
+recipes = {
+		"cobbled_deepslate": 1,
+		"polished_deepslate": 1,
+		"deepslate_bricks": 1,
+		"cracked_deepslate_bricks": 1,
+		"deepslate_tiles": 1,
+		"cracked_deepslate_bricks": 1,
+		"chiseled_deepslate": 1,
+		"cobbled_deepslate_wall": 1,
+		"polished_deepslate_wall": 1,
+		"deepslate_brick_wall": 1,
+		"deepslate_tile_wall": 1,
+		"cobbled_deepslate_stairs": 1,
+		"polished_deepslate_stairs": 1,
+		"deepslate_brick_stairs": 1,
+		"deepslate_tile_stairs": 1,
+		"cobbled_deepslate_slab": 2,
+		"polished_deepslate_slab": 2,
+		"deepslate_brick_slab": 2,
+		"deepslate_tile_slab": 2
+	}
 
-for recipe in recipes:
+for item, count in recipes.items():
 	dict = {
 		"type": "minecraft:stonecutting",
 		"ingredient": {
 			"item": "minecraft:deepslate"
 		},
-		"result": f"minecraft:{recipe}",
-		"count": 1
+		"result": f"minecraft:{item}",
+		"count": count
 	}
-	with open(f"data/minecraft/recipes/{recipe}_from_deepslate_stonecutting.json", "w") as f:
+	with open(f"data/minecraft/recipes/{item}_from_deepslate_stonecutting.json", "w") as f:
 		json.dump(dict, f, indent = "\t")
